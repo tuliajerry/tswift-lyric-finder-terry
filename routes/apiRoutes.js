b76e89985f741317d8ea6cc26fb65e2d59f36340
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-11-21 17:57:22 -0600

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,8 @@
 const router = require("express").Router();
 const controllers = require("../controllers");
 const checkAuth = require("../middleware/auth");
-const { searchSong } = require("../services/genius"); 
+const { searchSong } = require("../controllers/genius"); 
 
-
-router.post("/login", controllers.auth.login);
-router.get("/logout", controllers.auth.logout);
-router.post("/signup", controllers.user.create);
 
 
 router.get("/search", async (req, res) => {
