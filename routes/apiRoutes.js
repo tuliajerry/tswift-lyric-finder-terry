4cacf4f150e77b63954f45555f7b4010ad29d5d8
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-11-21 18:22:31 -0600

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,38 +1,26 @@
 const router = require("express").Router();
-const controllers = require("../controllers");
-const { searchSong } = require("../controllers/genius"); 
+const { searchSong } = require("../controllers/genius");  
 
 
 router.get("/search", async (req, res) => {
-   
     const songTitle = req.query.song; 
-
-    
     if (!songTitle) {
-        return res.status(400).send({ error: "Song title is required" });
+        return res.status(400).render('index', { error: "Song title is required" });  
     }
 
     try {
-        
         const songData = await searchSong(songTitle); 
 
-       
         if (songData) {
-            res.render('index', { 
-                song: songData, 
-                lyrics: songData.lyrics,
-                songTitle: songTitle 
-            });
+           
+            res.render('index', { song: songData, lyrics: songData.lyrics });
         } else {
-          
+           
             res.render('index', { error: 'Song not found' });
         }
     } catch (error) {
-  
         console.error("Error fetching song data:", error);
-
-     
-        res.render('index', { error: 'Something went wrong while searching for the song' });
+        res.render('index', { error: 'Something went wrong while searching for the song' });  
     }
 });
 
