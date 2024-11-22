5269c63776d86b583b0ab54f0019f54168de9c04
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-11-21 18:32:45 -0600

    Update genius.js

--- controllers/genius.js
+++ controllers/genius.js
@@ -1,9 +1,10 @@
 const axios = require("axios");
+const cheerio = require("cheerio");
 require('dotenv').config(); 
 
-
-const API_KEY = process.env.GENIUS_API_KEY;  
+const API_KEY = process.env.GENIUS_API_KEY; 
 const BASE_URL = "https://api.genius.com";
+
 
 const searchSong = async (songTitle) => {
   try {
@@ -18,37 +19,40 @@ const searchSong = async (songTitle) => {
       const song = hits[0].result;
 
      
-      const lyricsResponse = await axios.get(song.url);
-      const lyrics = extractLyricsFromPage(lyricsResponse.data);
+      const lyrics = await fetchLyrics(song.url);
 
-      
+     
       return {
         title: song.title,
         artist: song.primary_artist.name,
-        lyrics: lyrics,
-        album_artwork_url: song.song_art_image_url, 
+        album_artwork_url: song.song_art_image_url,
+        lyrics: lyrics || "Lyrics not found", 
+        url: song.url
       };
     } else {
       return null; 
     }
   } catch (error) {
-    console.error("Error fetching song data:", error);
+    console.error("Error fetching song data:", error.message);
     return null; 
   }
 };
 
 
-const extractLyricsFromPage = (html) => {
-  const regex = /<div class="lyrics">[\s\S]*?<p>([\s\S]*?)<\/p>/;
-  const match = html.match(regex);
-  if (match && match[1]) {
-    return match[1].replace(/<br\s*\/?>/g, "\n"); 
-  } else {
-    return "Lyrics not found"; 
+const fetchLyrics = async (url) => {
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data); 
+
+    
+    const lyrics = $("div[class^='Lyrics__Container']").text().trim();
+    return lyrics;
+  } catch (error) {
+    console.error("Error fetching lyrics:", error.message);
+    return null;
   }
 };
 
 module.exports = {
   searchSong,
 };
-
